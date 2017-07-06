c454be25aa3dc9c0459f7e14e6ab0919a008484b
Doug <doug.steinberg@gmail.com>
2017-07-06 15:40:04 -0400

    Error when submit empty forms

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
+// When the user presses the .submit button, add an .error class to both input elements
+// When the user puts their cursor in one of the input fields, remove the .error class
 
 document.addEventListener('DOMContentLoaded', function() {
 
   var signIn = document.querySelector('.signin');
   var modalWindow = document.querySelector('.modal');
-  var close = document.querySelector('.close');
-
   signIn.addEventListener('click', function(e){
     modalWindow.style.display = 'block';
   });
 
+  var close = document.querySelector('.close');
   close.addEventListener('click', function(e){
     modalWindow.style.display = 'none';
+  });
+
+  var submit = document.querySelector('.submit');
+  var input = document.querySelectorAll('input');
+  submit.addEventListener('click', function(e){
+    input[0].className = 'error';
+    input[1].className = 'error';
+
   });
 
 });
